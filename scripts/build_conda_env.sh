304e3ad2079e6ce294feaa46112ebed35408bf45
AaronFeller <aaronleefeller@gmail.com>
2022-10-21 12:32:00 -0500

    Notes

--- scripts/build_conda_env.sh
+++ scripts/build_conda_env.sh
@@ -1,4 +1,11 @@
-# Script for manual install of cinful environment
+#!/bin/bash
 
-mamba create  -n cinful 
+<< note
+Script is for manual install of the cinful conda environment.
+###### This method is not recommended!! #####
+See https://github.com/wilkelab/cinful for recommended install.
+note
+
+
+mamba create -n cinful 
 mamba env update -n cinful --file cinful_conda.yml
