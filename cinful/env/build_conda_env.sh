3f4c6d8d63449b0bd16bc92584490387a303d236
AaronFeller <aaronleefeller@gmail.com>
2022-10-27 17:05:18 -0500

    Env -- github fix

--- cinful/env/build_conda_env.sh
+++ cinful/env/build_conda_env.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-<< note
-Script is for manual install of the cinful conda environment.
-###### This method is not recommended!! #####
-See https://github.com/wilkelab/cinful for recommended install.
-note
-
-
-mamba create -n cinful 
+mamba create -n cinful
 mamba env update -n cinful --file cinful_conda.yml
